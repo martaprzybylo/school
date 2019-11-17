1f6854f71f373ed2103e62098db19463fc5c72dd
Marta Przybyło <marta.m.przybylo@gmail.com>
2019-11-17 14:23:42 +0100

    new schoolAplication

--- src/main/java/com/sda/school/SchoolApplication.java
+++ src/main/java/com/sda/school/SchoolApplication.java
@@ -27,25 +27,31 @@ public class SchoolApplication {
         s.setPerson(p);
         s.setGrades(Lists.newArrayList(new Grade(5,s), new Grade(5,s), new Grade(6,s)));
 
-        Student found = em.find(Student.class, 1L);
-        em.remove(found);
-        Student found2 = em.find(Student.class, 2L);
-        em.remove(found2);
-
-        Student found3 = em.find(Student.class, 3L);
-        found3.getPerson().setFirstName("Marta");
-        em.merge(found3);
+//        Student found = em.find(Student.class, 1L);
+//        em.remove(found);
+//        Student found2 = em.find(Student.class, 2L);
+//        em.remove(found2);
+//
+//        Student found3 = em.find(Student.class, 3L);
+//        found3.getPerson().setFirstName("Marta");
+//        em.merge(found3);
 
         em.persist(s);
         em.getTransaction().commit();
 
         String query = "SELECT s FROM STUDENT s WHERE s.person.firstName=:firstName AND s.person.lastName=:lastName";
-        List <Student> people =
-                em.createQuery(query)
-                        .setParameter("firstName", "Mysia").setParameter("lastName","Pysia")
-                        .getResultList();
+//        List <Student> people =
+//                em.createQuery(query)
+//                        .setParameter("firstName", "Mysia").setParameter("lastName","Pysia")
+//                        .getResultList();
+//
+//        System.out.println(people);
+
+        List<Student> people = em.createQuery("SELECT s FROM Student s Where s.person.firstName=:parametr ")
+                .setParameter("parametr","Mysia").getResultList();
         
-        System.out.println(people);
+
+
 
 
 
